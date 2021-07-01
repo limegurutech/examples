2d849aacadef5a3c7db6a204cfba2d1a8730c20d
Deepak Baweja <deepak.baweja@aexp.com>
2021-07-01 10:42:14 +0530

    json data postgres queries

--- postgres/jsonb/commands.sql
+++ postgres/jsonb/commands.sql
@@ -12,9 +12,24 @@ INSERT INTO emp VALUES
 
 
 select data from emp;
+
+
 select data->'name' from emp;
 SELECT * FROM emp WHERE data->'name' = '"Marc"'; 
 select data->'hobbies' from emp;
 SELECT data->'hobbies' FROM emp WHERE data->'name' = '"Marc"';
+
+SELECT jsonb_array_elements_text(data->'hobbies') AS hobbies  FROM emp;
+SELECT distinct(jsonb_array_elements_text(data->'hobbies')) AS hobbies  FROM emp;
+
+SELECT emp_id, jsonb_array_elements_text(data->'hobbies') AS hobbies  FROM emp;
+
+SELECT emp_id, data FROM emp WHERE data->'hobbies' @> '["Gaming"]'::jsonb; 
+SELECT emp_id, data FROM emp WHERE data->'hobbies' @> '["Movies"]'::jsonb; 
+
+
+drop table emp;
+
+
 
 
